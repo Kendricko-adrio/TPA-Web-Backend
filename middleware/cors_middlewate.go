09acd07641f1d0b0d726ea206a510d06092ac926
Kendricko-adrio <kendrickoadrio134@gmail.com>
2021-03-13 12:25:59 +0700

    seed udah diganti linknya

--- middleware/cors_middlewate.go
+++ middleware/cors_middlewate.go
@@ -5,8 +5,8 @@ import "net/http"
 func CorsMiddleware(next http.Handler) http.Handler{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		//w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
-		w.Header().Set("Access-Control-Allow-Origin", "https://604c2ca83849e54be83743fc--suspicious-engelbart-00f72e.netlify.app")
+		//w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")https://604c4b7ea9112e6d754fa318--suspicious-engelbart-00f72e.netlify.app/
+		w.Header().Set("Access-Control-Allow-Origin", "https://604c4b7ea9112e6d754fa318--suspicious-engelbart-00f72e.netlify.app")
 		//w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:8080")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "POST")
